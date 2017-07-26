787c16ed417db33c1d4fcf914d71b18cac037e30
Vadym Grabovyi <soyer@macpaw.com>
2017-07-26 19:24:08 +0300

    product page

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,29 @@
-console.log('test');
+import { lory } from 'lory.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // slider
+  const slider = document.querySelector('.js_slider');
+  if (slider) {
+    lory(slider);
+  }
+  // expanede block
+  (() => {
+    const block = document.querySelectorAll('.js-show');
+    [].slice.call(block).forEach((item) => {
+      const btn = item.querySelector('.js-show-more');
+      btn.addEventListener('click', () => {
+        item.classList.add('is-open');
+      });
+    });
+  })();
+  // hint
+  (() => {
+    const hint = document.querySelectorAll('.hint');
+    [].slice.call(hint).forEach((item) => {
+      const btn = item.querySelector('.hint-btn');
+      btn.addEventListener('click', () => {
+        item.classList.add('is-open');
+      });
+    });
+  })();
+});
